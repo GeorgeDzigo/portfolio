3d34de411ab8738c5c76008e410549af91dc246e
GeorgeDzigo <mr.georgegamertv@gmail.com>
2021-02-24 23:27:31 +0200

    Finished The Web-Site structure

--- js/home.js
+++ js/home.js
@@ -1,3 +1,4 @@
+// This is portfolio works/projects
 function works() {
       let data = [
             {
@@ -44,17 +45,19 @@ function works() {
 
       let btn = document.querySelectorAll('.timeline-works-holder-work');
       let toD = document.querySelectorAll('.timeline-works-holder-work-headings');
-      console.log(toD);
       for (let i = 0; i < btn.length; i++) {
             btn[i].addEventListener('mouseover', function () { 
-                  toD[i].style.display = "block";
+                  toD[i].style.transform = "translate(0px, -165px)";
+                  toD[i].style.transition = ".85s ease";
             });
             btn[i].addEventListener("mouseout", function () {
-                  toD[i].style.display = "none";
+                  toD[i].style.transform = "translate(0px, 0px)";
+                  toD[i].style.transition = ".85s ease";
             });
       }
 }
 works();
+// This is my education history
 function education() { 
       let data = [
             {
@@ -109,7 +112,7 @@ function education() {
 }
 education();
 
-
+// This is my work history
 function work() { 
       let data = [
             {
@@ -163,3 +166,131 @@ function work() {
       }
 }
 work();
+
+// This is my services 
+function services() {
+      let data = [
+            {
+                  "title": "Web Development",
+                  "desc": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores ratione neque expedita sint tempora animi corporis veritatis dolor! Deleniti unde similique officiis eos commodi harum labore quis voluptatem molestias assumenda."
+            },
+            {
+                  "title": "Full Stack",
+                  "desc": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores ratione neque expedita sint tempora animi corporis veritatis dolor! Deleniti unde similique officiis eos commodi harum labore quis voluptatem molestias assumenda."
+            },
+            {
+                  "title": "Front End",
+                  "desc": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores ratione neque expedita sint tempora animi corporis veritatis dolor! Deleniti unde similique officiis eos commodi harum labore quis voluptatem molestias assumenda."
+            },
+            {
+                  "title": "Back End",
+                  "desc": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores ratione neque expedita sint tempora animi corporis veritatis dolor! Deleniti unde similique officiis eos commodi harum labore quis voluptatem molestias assumenda."
+            },
+            {
+                  "title": "UI/UX Design",
+                  "desc": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores ratione neque expedita sint tempora animi corporis veritatis dolor! Deleniti unde similique officiis eos commodi harum labore quis voluptatem molestias assumenda."
+            },
+            {
+                  "title": "Web Design",
+                  "desc": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores ratione neque expedita sint tempora animi corporis veritatis dolor! Deleniti unde similique officiis eos commodi harum labore quis voluptatem molestias assumenda."
+            },
+      ];
+
+      let sh = document.querySelector(".timeline-services");
+      for (let i = 0; i < data.length; i++) {
+            sh.innerHTML += `
+            
+            <div class="timeline-service-box" id="box">
+            <div class="timeline-service-box-headings">
+                  <h1 class="timeline-service-box-h1">${data[i]["title"]}</h1>
+                  <p class="timeline-services-box-p">
+                        ${data[i]["desc"]}
+                  </p>
+                  <a href="#" class="timeline-services-box-a">
+                        ORDER NOW  >
+                  </a>
+            </div>
+      </div>
+            
+            `;
+      }
+}
+services();
+
+// This Is My Recommendations
+function recoms() {
+      let data = [
+            {
+                  "img": "images/recom/1.jpg",
+                  "name": "Paul Trueman",
+                  "specialty": "Template Author",
+                  "recom" : "Working with Artur has been a pleasure. Better yet - I alerted them of a minor issue before going to sleep. The issue was fixed the next morning. I couldn't ask for better support. Thank you Artur! This is easily a 5 star freelancer.",
+                  "rating" : "XXXXX"
+            },
+            {
+                  "img": "images/recom/2.jpg",
+                  "name": "Paul Trueman",
+                  "specialty": "Template Author",
+                  "recom" : "Working with Artur has been a pleasure. Better yet - I alerted them of a minor issue before going to sleep. The issue was fixed the next morning. I couldn't ask for better support. Thank you Artur! This is easily a 5 star freelancer.",
+                  "rating" : "XXXXX"
+            },
+            {
+                  "img": "images/recom/3.jpg",
+                  "name": "Paul Trueman",
+                  "specialty": "Template Author",
+                  "recom" : "Working with Artur has been a pleasure. Better yet - I alerted them of a minor issue before going to sleep. The issue was fixed the next morning. I couldn't ask for better support. Thank you Artur! This is easily a 5 star freelancer.",
+                  "rating" : "XXXXX"
+            },
+            {
+                  "img": "images/recom/4.jpg",
+                  "name": "Paul Trueman",
+                  "specialty": "Template Author",
+                  "recom" : "Working with Artur has been a pleasure. Better yet - I alerted them of a minor issue before going to sleep. The issue was fixed the next morning. I couldn't ask for better support. Thank you Artur! This is easily a 5 star freelancer.",
+                  "rating" : "XXXXX"
+            },
+      ];
+
+      let rh = document.querySelector(".recoms-holder");
+
+      for (let i = 0; i < data.length; i++) {
+            rh.innerHTML += `
+            <div class="recoms-holder-box" id="box">
+                  <img src="${data[i]["img"]}" class="recoms-holder-recom-img">
+                  <div class="recoms-holder-recom-headings">
+                        <h1 class="recoms-holder-recom-headings-name">${data[i]["name"]}</h1>
+                        <h2 class="recoms-holder-recom-headings-specialty">${data[i]["specialty"]}</h2>
+                        <p class="recoms-holder-recom-headings-recomendation">${data[i]["recom"]}</p>
+                        <div class="recoms-holder-recom-headings-rating">${data[i]["rating"]}</div>
+                  </div>
+            </div>
+            `;
+      }
+}
+recoms()
+
+// This Is Slider Function 
+function slider() { 
+
+      let left = document.querySelector(".recoms-arrow-left");
+      let right = document.querySelector(".recoms-arrow-right");
+      let slider = document.querySelector(".recoms-holder");
+      let i = 0;
+      let trans = 0;
+      right.addEventListener('click', function () {
+            if (i != 2) {
+                  i++;
+                  trans -= 545;
+                  slider.style.transform = "translate("+trans+"px, 0)";
+                  slider.style.transition = ".85s ease";
+            }
+      });
+      left.addEventListener('click', function () {
+            if (i != 0) {
+                  i--;
+                  trans += 545;
+                  slider.style.transform = "translate("+trans+"px, 0)";
+                  slider.style.transition = ".85s ease";
+            }
+      });
+}
+slider();
